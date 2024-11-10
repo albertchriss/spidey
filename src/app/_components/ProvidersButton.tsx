dd702a0493897e76765ec6684cdd462cbf207e3b
albertchriss <19623210@std.stei.itb.ac.id>
2024-11-10 11:06:00 +0700

    fix: deploy error again

--- src/app/_components/ProvidersButton.tsx
+++ src/app/_components/ProvidersButton.tsx
@@ -1,17 +1,21 @@
+"use server";
 
 import { Button } from "~/components/ui/button";
 import { signIn } from "~/server/auth";
 
 interface ProvidersButtonProps {
-    provider: string;
+  provider: string;
 }
-export const ProvidersButton = ({provider} : ProvidersButtonProps) => {
-    return (
-        <form action={async () => {
-            "use server"
-            await signIn(provider, {redirectTo: "/tasklist"})
-        }}>
-            <Button size="lg" type="submit">Sign In</Button>
-        </form>
-    )
-}
+export const ProvidersButton = ({ provider }: ProvidersButtonProps) => {
+  return (
+    <form
+      action={async () => {
+        await signIn(provider, { redirectTo: "/tasklist" });
+      }}
+    >
+      <Button size="lg" type="submit">
+        Sign In
+      </Button>
+    </form>
+  );
+};
--- src/app/_components/SignOutButton.tsx
+++ src/app/_components/SignOutButton.tsx
@@ -1,9 +1,9 @@
+"use server"
 import { signOut } from "~/server/auth"
 
 export const SignOutButton = () => {
     return (
         <form action={async () => {
-            "use server"
             await signOut({redirectTo: "/"})
         }}>
             <button type="submit">Sign Out</button>
